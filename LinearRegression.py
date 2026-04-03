bce504a34efa083f0f72b129d6f66dc3203879a6
NITHI2209 <nithyasree065@gmail.com>
2026-04-03 20:53:53 +0530

    created

---
+++ LinearRegression.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.datasets import load_iris
+X,y=load_iris(return_X_y=True)
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X,y)
+pred = model.predict(X)
+plt.scatter(pred,y)
+plt.show()
+#Refer Figure 1
+
