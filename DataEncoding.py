6f3af7555f37a96d6f0437f82148361c652b8754
NITHI2209 <nithyasree065@gmail.com>
2026-04-06 20:02:14 +0530

    created

---
+++ DataEncoding.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+df = fetch_openml("titanic",version= 1 , as_frame=True)["data"]
+from sklearn.preprocessing import OneHotEncoder
+df[["female","male"]] = OneHotEncoder().fit_transform(df[["sex"]]).toarray()
+# print(df[["sex","female","male"]])
